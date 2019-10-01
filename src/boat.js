3728a536a3c712568b2608927c4f8a48e23106ff
Rebecca Lambert <rebeccalambert@berkeley.edu>
2019-10-01 15:27:48 -0700

    Correct boats' direction

--- src/boat.js
+++ src/boat.js
@@ -8,32 +8,14 @@ function Boat (pos, game) {
   this.COLOR = "grey";
   this.RADIUS = 15;
   this.RANDOMVECTOR = Util.randomVec(Math.random()*3);
-  MovingObject.call(this,{pos: pos.pos, vel: this.RANDOMVECTOR, color: this.COLOR, radius: this.RADIUS, game: game});
+  MovingObject.call(this,{pos: pos.pos, vel: [Util.getRandomInt(6), 0], color: this.COLOR, radius: this.RADIUS, game: game});
 }
 
 Boat.prototype.collideWith = function (otherObject) {
   if (otherObject instanceof Ship) {
     otherObject.relocate();
   }
-  // if (otherObject instanceof Boat) {
-  //   this.shiftDirection();
-// }
 };
-
-// Boat.prototype.shiftDirection = function () {
-  // if ((this.vel[0] < 0) && (this.vel[1] > 0)) {
-  //   this.vel[1] = this.vel - .1
-  // }
-  // if ((this.vel[0] < 0) && (this.vel[1] < 0)) {
-  //   this.vel[0] = this.vel - .1
-  // }
-  // if ((this.vel[0] > 0) && (this.vel[1] > 0)) {
-  //   this.vel[0] = this.vel - .1
-  // }
-  // if ((this.vel[0] < 0) && (this.vel[1] < 0)) {
-  //   this.vel[1] = this.vel - .1
-  // }
-// }
 
 Boat.prototype.avoidBoats = function (otherObject) {
   
